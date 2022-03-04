04303ac761f25fab5b97ecfc868f43d35e4144ec
Shigang Li <shigangli.cs@gmail.com>
2022-03-04 10:33:20 +0100

    test2

--- MagicCube/compile.sh
+++ MagicCube/compile.sh
@@ -1,1 +1,1 @@
-nvcc u4_mma.cu -ccbin g++ -m64 -gencode arch=compute_80,code=compute_80 -I./Common -o test1 -Dverify_output
+nvcc u4_mma.cu -ccbin g++ -m64 -gencode arch=compute_80,code=compute_80 -I./Common -o test2 -Dverify_output
