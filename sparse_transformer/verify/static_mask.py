0ab122b0828314165f4cebc15d24465118522c20
Shigang Li <shigangli.cs@gmail.com>
2022-03-21 20:15:13 +0100

    format

--- sparse_transformer/verify/static_mask.py
+++ sparse_transformer/verify/static_mask.py
@@ -26,6 +26,56 @@ def static_random_mask(m, n, sparsity):
     return column_indices, row_offsets, row_indices
 
 
+def static_random_mask_aligned(m, n, sparsity, mma_k_dim):
+
+    csr_mask = random(m=m, n=n, density = 1. - sparsity, format='csr')
+    column_indices = csr_mask.indices
+    row_offsets = csr_mask.indptr
+
+    aligned_num_item = 0
+    aligned_row_offsets = np.zeros(m*2, dtype='int32')
+    aligned_row_offsets[0] = aligned_num_item
+    for i in range(1, m+1):
+        num_item = row_offsets[i] - row_offsets[i-1]
+        aligned_num_item += int((num_item + mma_k_dim - 1) / mma_k_dim) * mma_k_dim
+        if i != m:
+            aligned_row_offsets[i*2] = aligned_num_item
+        aligned_row_offsets[i*2-1] = aligned_row_offsets[i*2-2] + num_item
+
+    row_length = []
+
+    for i in range(m):
+        row_length.append(row_offsets[i+1] - row_offsets[i])
+
+    row_indices = np.argsort(row_length)
+
+    aligned_col_indices = np.zeros(aligned_num_item, dtype='int32')
+    aligned_col_indices_shuffle = np.zeros(aligned_num_item, dtype='int32')
+
+    for i in range(aligned_num_item):
+        aligned_col_indices[i] = -1
+        aligned_col_indices_shuffle[i] = -1
+
+    for i in range(1, m+1):
+        offset_begin = row_offsets[i-1]
+        offset_end = row_offsets[i]
+        for j in range(offset_begin, offset_end):
+            aligned_col_indices[aligned_row_offsets[(i-1)*2] + j - offset_begin] = column_indices[j]
+
+    chunks = int(aligned_num_item/8)
+
+    for i in range(chunks):
+        for j in range(8):
+            aligned_col_indices_shuffle[i*8 + (j%2)*4 + int(j/2)] = aligned_col_indices[i*8 + j]
+
+    column_indices = torch.tensor(aligned_col_indices, dtype=torch.int32, device='cuda')
+    column_indices_shuffle = torch.tensor(aligned_col_indices_shuffle, dtype=torch.int32, device='cuda')
+    row_offsets = torch.tensor(aligned_row_offsets, dtype=torch.int32, device='cuda')
+    row_indices = torch.tensor(row_indices, dtype=torch.int32, device='cuda')
+
+    return column_indices, column_indices_shuffle, row_offsets, row_indices, aligned_num_item
+
+
 def csr2dense(column_indices, row_offsets, values, m, n, vec_length, val=0.):
     column_indices_cpu = column_indices.cpu().detach().numpy()
     row_offsets_cpu = row_offsets.cpu().detach().numpy()
