d789a116b254dc27a8729f016b4d8b178a1d16ba
Shigang Li <shigangli.cs@gmail.com>
2022-04-10 21:01:49 +0000

    sddmm final

--- SDDMM/SDDMM/launch_sddmm_8b8b.py
+++ SDDMM/SDDMM/launch_sddmm_8b8b.py
@@ -1,9 +1,8 @@
 import os
 import argparse
-import numpy as np
 
 # Args
-parser = argparse.ArgumentParser(description='lauch the spmm benchmarks')
+parser = argparse.ArgumentParser(description='lauch the sddmm benchmarks')
 
 #parser.add_argument('--dimK', type=int, default=256, help="the dimension N of the benchmark")
 #parser.add_argument('--dimV', type=int, default=8, help="vector length")
@@ -12,6 +11,7 @@ parser = argparse.ArgumentParser(description='lauch the spmm benchmarks')
 #parser.add_argument('--preB', type=int, default=8, help="number of bits for B")
 args = parser.parse_args()
 
+dataset_dir = '/users/shigang/gitrepo/dlmc/'
 sparsities = ['50', '70', '80', '90', '95', '98']
 dimKs = [128, 256]
 vec_lens = [2, 4, 8]
@@ -21,13 +21,11 @@ for dimK in dimKs:
         for sparsity in sparsities:
             print("dimK: ", dimK, "vec_len: ", vec_len, "sparsity: ", sparsity)
         
-            matrix_list = open('/users/shigang/gitrepo/dlmc/s%s.txt' % sparsity, 'r')
+            matrix_list = open('./eval_matrices/s%s.txt' % sparsity, 'r')
             lines = matrix_list.readlines()
-            #print("number_of_matrix: ", len(lines))
             #for i in range(1):
             for i in range(len(lines)):
-                matrix = '/users/shigang/gitrepo/dlmc/%s' % lines[i][:-1]
-                #print(matrix)
+                matrix = '%s%s' % (dataset_dir, lines[i][:-1])
                 cmd = './sddmm_benchmark %s %d %d 1 0 1 8 8' % (matrix, dimK, vec_len)
                 os.system(cmd)
 
