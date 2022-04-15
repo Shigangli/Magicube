9651f675d1cae9ee77377bf70e668db89dc90f1d
Shigang Li <shigangli.cs@gmail.com>
2022-04-15 19:16:46 +0000

    sddmm abl

--- SDDMM/ablation_study/compile_jobs.sh
+++ SDDMM/ablation_study/compile_jobs.sh
@@ -1,8 +1,10 @@
 cd ./SDDMM_basic
+chmod 777 setup.sh
 ./setup.sh
 echo "SDDMM basic is compiled."
 
 cd -
 cd ./SDDMM_lhs_pref
+chmod 777 setup.sh
 ./setup.sh
 echo "SDDMM with LHS prefetch is compiled."
