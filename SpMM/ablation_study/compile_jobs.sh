be1bb29b6464ddb84dbb6aba7ff94348f46b9d68
Shigang Li <shigangli.cs@gmail.com>
2022-04-15 19:07:43 +0000

    777

--- SpMM/ablation_study/compile_jobs.sh
+++ SpMM/ablation_study/compile_jobs.sh
@@ -1,46 +1,57 @@
 cd ./SpMM_basic
+chmod 777 setup.sh
 ./setup.sh
 echo "SpMM basic compiled."
 
 cd -
 cd ./16b8b/SpMM_conflict_free
+chmod 777 setup.sh
 ./setup.sh
 
 cd -
 cd ./8b8b/SpMM_conflict_free
+chmod 777 setup.sh
 ./setup.sh
 
 cd -
 cd ./8b4b/SpMM_conflict_free
+chmod 777 setup.sh
 ./setup.sh
 
 cd -
 cd ./4b4b/SpMM_conflict_free
+chmod 777 setup.sh
 ./setup.sh
 echo "SpMM with conflict-free SM compiled."
 
 cd -
 cd ./16b8b/SpMM_conflict_free_prefetch
+chmod 777 setup.sh
 ./setup.sh
 
 cd -
 cd ./8b8b/SpMM_conflict_free_prefetch
+chmod 777 setup.sh
 ./setup.sh
 
 cd -
 cd ./8b4b/SpMM_conflict_free_prefetch
+chmod 777 setup.sh
 ./setup.sh
 
 cd -
 cd ./4b4b/SpMM_conflict_free_prefetch
+chmod 777 setup.sh
 ./setup.sh
 echo "SpMM with conflict-free SM + prefetch compiled."
 
 cd -
 cd ./8b4b/SpMM_conflict_free_prefetch_shuffle
+chmod 777 setup.sh
 ./setup.sh
 
 cd -
 cd ./4b4b/SpMM_conflict_free_prefetch_shuffle
+chmod 777 setup.sh
 ./setup.sh
 echo "SpMM with conflict-free SM + prefetch + shuffle compiled."
--- SpMM/ablation_study/spmm_throughput_eval.sh
+++ SpMM/ablation_study/spmm_throughput_eval.sh
@@ -1,46 +1,57 @@
 cd ./SpMM_basic
+chmod 777 run_jobs.sh
 ./run_jobs.sh
 echo "SpMM basic tested."
 
 cd -
 cd ./16b8b/SpMM_conflict_free
+chmod 777 run_jobs.sh
 ./run_jobs.sh
 
 cd -
 cd ./8b8b/SpMM_conflict_free
+chmod 777 run_jobs.sh
 ./run_jobs.sh
 
 cd -
 cd ./8b4b/SpMM_conflict_free
+chmod 777 run_jobs.sh
 ./run_jobs.sh
 
 cd -
 cd ./4b4b/SpMM_conflict_free
+chmod 777 run_jobs.sh
 ./run_jobs.sh
 echo "SpMM with conflict-free SM tested."
 
 cd -
 cd ./16b8b/SpMM_conflict_free_prefetch
+chmod 777 run_jobs.sh
 ./run_jobs.sh
 
 cd -
 cd ./8b8b/SpMM_conflict_free_prefetch
+chmod 777 run_jobs.sh
 ./run_jobs.sh
 
 cd -
 cd ./8b4b/SpMM_conflict_free_prefetch
+chmod 777 run_jobs.sh
 ./run_jobs.sh
 
 cd -
 cd ./4b4b/SpMM_conflict_free_prefetch
+chmod 777 run_jobs.sh
 ./run_jobs.sh
 echo "SpMM with conflict-free SM + prefetch tested."
 
 cd -
 cd ./8b4b/SpMM_conflict_free_prefetch_shuffle
+chmod 777 run_jobs.sh
 ./run_jobs.sh
 
 cd -
 cd ./4b4b/SpMM_conflict_free_prefetch_shuffle
+chmod 777 run_jobs.sh
 ./run_jobs.sh
 echo "SpMM with conflict-free SM + prefetch + shuffle tested."
