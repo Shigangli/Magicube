406e4c43ee5b70a8a225b854123f910fdae1a71e
Shigang Li <shigangli.cs@gmail.com>
2022-04-15 19:05:37 +0000

    SpMM dataset path

--- SpMM/ablation_study/16b8b/SpMM_conflict_free_prefetch/run_jobs.sh
+++ SpMM/ablation_study/16b8b/SpMM_conflict_free_prefetch/run_jobs.sh
@@ -2,11 +2,11 @@
 echo -e "Evaluation perf for different precisions: N = 512, Iteration = 1024 \n"
 
 echo -e "L16-R8 \n"
-./spmm_benchmark  /users/shigang/gitrepo/dlmc/rn50/random_pruning/0.7/bottleneck_2_block_group3_5_1.smtx 512 2 0 1 1 1 16 8
+./spmm_benchmark  ${dataset_dir}/rn50/random_pruning/0.7/bottleneck_2_block_group3_5_1.smtx 512 2 0 1 1 1 16 8
 echo -e "\n"
-./spmm_benchmark  /users/shigang/gitrepo/dlmc/rn50/random_pruning/0.7/bottleneck_2_block_group3_5_1.smtx 512 8 0 1 1 1 16 8
+./spmm_benchmark  ${dataset_dir}/rn50/random_pruning/0.7/bottleneck_2_block_group3_5_1.smtx 512 8 0 1 1 1 16 8
 echo -e "\n"
-./spmm_benchmark  /users/shigang/gitrepo/dlmc/rn50/random_pruning/0.9/bottleneck_2_block_group3_5_1.smtx 512 2 0 1 1 1 16 8
+./spmm_benchmark  ${dataset_dir}/rn50/random_pruning/0.9/bottleneck_2_block_group3_5_1.smtx 512 2 0 1 1 1 16 8
 echo -e "\n"
-./spmm_benchmark  /users/shigang/gitrepo/dlmc/rn50/random_pruning/0.9/bottleneck_2_block_group3_5_1.smtx 512 8 0 1 1 1 16 8
+./spmm_benchmark  ${dataset_dir}/rn50/random_pruning/0.9/bottleneck_2_block_group3_5_1.smtx 512 8 0 1 1 1 16 8
 echo -e "\n"
--- SpMM/ablation_study/8b4b/SpMM_conflict_free/run_jobs.sh
+++ SpMM/ablation_study/8b4b/SpMM_conflict_free/run_jobs.sh
@@ -2,12 +2,12 @@
 echo -e "Evaluation perf for different precisions: N = 512, Iteration = 1024 \n"
 
 echo -e "L8-R4 \n"
-./spmm_benchmark  /users/shigang/gitrepo/dlmc/rn50/random_pruning/0.7/bottleneck_2_block_group3_5_1.smtx 512 2 0 1 1 1 8 4
+./spmm_benchmark  ${dataset_dir}/rn50/random_pruning/0.7/bottleneck_2_block_group3_5_1.smtx 512 2 0 1 1 1 8 4
 echo -e "\n"
-./spmm_benchmark  /users/shigang/gitrepo/dlmc/rn50/random_pruning/0.7/bottleneck_2_block_group3_5_1.smtx 512 8 0 1 1 1 8 4
+./spmm_benchmark  ${dataset_dir}/rn50/random_pruning/0.7/bottleneck_2_block_group3_5_1.smtx 512 8 0 1 1 1 8 4
 echo -e "\n"
-./spmm_benchmark  /users/shigang/gitrepo/dlmc/rn50/random_pruning/0.9/bottleneck_2_block_group3_5_1.smtx 512 2 0 1 1 1 8 4
+./spmm_benchmark  ${dataset_dir}/rn50/random_pruning/0.9/bottleneck_2_block_group3_5_1.smtx 512 2 0 1 1 1 8 4
 echo -e "\n"
-./spmm_benchmark  /users/shigang/gitrepo/dlmc/rn50/random_pruning/0.9/bottleneck_2_block_group3_5_1.smtx 512 8 0 1 1 1 8 4
+./spmm_benchmark  ${dataset_dir}/rn50/random_pruning/0.9/bottleneck_2_block_group3_5_1.smtx 512 8 0 1 1 1 8 4
 echo -e "\n"
 
