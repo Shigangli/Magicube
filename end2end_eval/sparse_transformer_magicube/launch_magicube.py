65dc63d894fabc61c8df478e5cf53763f9bdbe83
Shigang Li <shigangli.cs@gmail.com>
2022-06-04 10:12:59 +0200

    n2n

--- end2end_eval/sparse_transformer_magicube/launch_magicube.py
+++ end2end_eval/sparse_transformer_magicube/launch_magicube.py
@@ -1,25 +1,329 @@
 import os
 
-for i in range(256):
-    cmd = 'python3 end_to_end.py --model sparse --bs 2 --lhs_pre 4 --rhs_pre 4'
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 2 --lhs_pre 4 --rhs_pre 4'
     os.system(cmd)
+print(cmd, flush=True)
 
-for i in range(256):
-    cmd = 'python3 end_to_end.py --model sparse --bs 2 --lhs_pre 8 --rhs_pre 8'
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 2 --lhs_pre 8 --rhs_pre 4'
     os.system(cmd)
+print(cmd, flush=True)
 
-for i in range(256):
-    cmd = 'python3 end_to_end.py --model sparse --bs 2 --lhs_pre 16 --rhs_pre 8'
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 2 --lhs_pre 8 --rhs_pre 8'
     os.system(cmd)
+print(cmd, flush=True)
 
-for i in range(256):
-    cmd = 'python3 end_to_end.py --model sparse --bs 8 --lhs_pre 4 --rhs_pre 4'
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 2 --lhs_pre 16 --rhs_pre 8'
     os.system(cmd)
+print(cmd, flush=True)
 
-for i in range(256):
-    cmd = 'python3 end_to_end.py --model sparse --bs 8 --lhs_pre 8 --rhs_pre 8'
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 8 --lhs_pre 4 --rhs_pre 4'
     os.system(cmd)
+print(cmd, flush=True)
 
-for i in range(256):
-    cmd = 'python3 end_to_end.py --model sparse --bs 8 --lhs_pre 16 --rhs_pre 8'
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 8 --lhs_pre 8 --rhs_pre 4'
     os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 8 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 8 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 2 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 2 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 2 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 2 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 8 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 8 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 8 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 8 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 2 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 2 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 2 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 2 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 8 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 8 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 8 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 8 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 2 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 2 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 2 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 2 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 8 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 8 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 8 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.9 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 8 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+
+
+
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 2 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 2 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 2 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 2 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 8 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 8 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 8 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 8 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 2 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 2 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 2 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 2 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 8 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 8 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 8 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 8 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+
+
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 2 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 2 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 2 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 2 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 8 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 8 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 8 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 8 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 2 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 2 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 2 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 2 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 8 --lhs_pre 4 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 8 --lhs_pre 8 --rhs_pre 4'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 8 --lhs_pre 8 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model sparse --sparsity 0.95 --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 8 --lhs_pre 16 --rhs_pre 8'
+    os.system(cmd)
+print(cmd, flush=True)
