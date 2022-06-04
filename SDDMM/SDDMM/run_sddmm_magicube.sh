afe4d677804b3c40b3d9b3d3a6597da9cdd27430
Shigang Li <shigangli.cs@gmail.com>
2022-06-05 00:27:50 +0200

    run_sddmm_magicube

---
+++ SDDMM/SDDMM/run_sddmm_magicube.sh
@@ -0,0 +1,12 @@
+
+echo "Tesing sddmm_magicube_16b16b"
+python launch_sddmm_magicube_16b16b.py > sddmm_magicube_16b16b.txt
+echo "Finish sddmm_magicube_16b16b"
+
+echo "Tesing sddmm_magicube_8b8b"
+python launch_sddmm_magicube_8b8b.py > sddmm_magicube_8b8b.txt
+echo "Finish sddmm_magicube_8b8b"
+
+echo "Tesing sddmm_magicube_4b4b"
+python launch_sddmm_magicube_4b4b.py > sddmm_magicube_4b4b.txt
+echo "Finish sddmm_magicube_4b4b"
