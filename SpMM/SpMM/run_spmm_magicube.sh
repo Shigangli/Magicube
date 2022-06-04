2aa41c317167d2b1e48a261bc57b47cf75b1b451
Shigang Li <shigangli.cs@gmail.com>
2022-06-05 00:23:24 +0200

    run_spmm_magicube

---
+++ SpMM/SpMM/run_spmm_magicube.sh
@@ -0,0 +1,16 @@
+
+echo "Tesing spmm_magicube_16b8b"
+python launch_spmm_magicube_16b8b.py > spmm_magicube_16b8b.txt
+echo "Finish spmm_magicube_16b8b"
+
+echo "Tesing spmm_magicube_8b8b"
+python launch_spmm_magicube_8b8b.py > spmm_magicube_8b8b.txt
+echo "Finish spmm_magicube_8b8b"
+
+echo "Tesing spmm_magicube_8b4b"
+python launch_spmm_magicube_8b4b.py > spmm_magicube_8b4b.txt
+echo "Finish spmm_magicube_8b4b"
+
+echo "Tesing spmm_magicube_4b4b"
+python launch_spmm_magicube_4b4b.py > spmm_magicube_4b4b.txt
+echo "Finish spmm_magicube_4b4b"
