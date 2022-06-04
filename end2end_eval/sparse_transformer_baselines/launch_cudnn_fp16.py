9f5d8bdb3cf940e479cf7766f265bb4168d3f8da
Shigang Li <shigangli.cs@gmail.com>
2022-06-04 10:16:52 +0200

    n2n baseline

--- end2end_eval/sparse_transformer_baselines/launch_cudnn_fp16.py
+++ end2end_eval/sparse_transformer_baselines/launch_cudnn_fp16.py
@@ -1,10 +1,42 @@
 import os
 
 
-for i in range(256):
-    cmd = 'python3 end_to_end.py --model dense --bs 2'
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model dense --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 2'
     os.system(cmd)
+print(cmd, flush=True)
 
-for i in range(256):
-    cmd = 'python3 end_to_end.py --model dense --bs 8'
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model dense --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 4096 --bs 8'
     os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model dense --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 2'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model dense --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 4096 --bs 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model dense --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 2'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model dense --mlp_dim 1024 --embed_dim 256 --num_heads 4 --seq_len 8192 --bs 8'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model dense --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 2'
+    os.system(cmd)
+print(cmd, flush=True)
+
+for i in range(16):
+    cmd = 'CUDA_VISIBLE_DEVICES=GPU-31acddbe-f963-b876-2508-0c529c73da36 python3 end_to_end.py --model dense --mlp_dim 2048 --embed_dim 512 --num_heads 8 --seq_len 8192 --bs 8'
+    os.system(cmd)
+print(cmd, flush=True)
