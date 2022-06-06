3716e26bf69ceec9e92453d0c5b8be80c49eb56a
Shigang Li <shigangli.cs@gmail.com>
2022-06-06 02:00:53 +0200

    f16

---
+++ plot/plot.sh
@@ -0,0 +1,8 @@
+python plot_n2n_a.py
+python plot_n2n_b.py
+python plot_n2n_c.py
+python plot_n2n_d.py
+python plot_n2n_e.py
+python plot_n2n_f.py
+python plot_n2n_g.py
+python plot_n2n_h.py
--- plot/plot_n2n_h.py
+++ plot/plot_n2n_h.py
@@ -8,16 +8,15 @@ import pandas as pd
 #sns.set_context(rc = {'patch.linewidth': 0.0})
 order = ['Pytorch-fp16', 'vectorSparse-fp16', 'Magicube-16b8b', 'Magicube-8b8b', 'Magicube-8b4b', 'Magicube-4b4b']
 
-n2n_a_data = pd.read_csv('n2n_a.csv')
+
+n2n_h_data = pd.read_csv('n2n_h.csv')
 sns.set(rc={"lines.linewidth": 0.5})
 sns.set(rc={'figure.figsize':(5, 3)})
-g = sns.barplot(data=n2n_a_data, x="S0.9,Seq_l=4096,num_h=4", y="Latency(ms)", hue="algs", palette="Blues_d", hue_order=order, ci=95, capsize=.1, errwidth=0.8)
-#plt.xticks(rotation=20)
+g = sns.barplot(data=n2n_h_data, x="S0.95,Seq_l=8192,num_h=8", y="Latency(ms)", hue="algs", palette="Blues_d", hue_order=order, ci=95, capsize=.1, errwidth=0.8)
 g.tick_params(labelsize=8)
-g.set(ylim=(0, 25))
+g.set(ylim=(0, 150))
 g.set_xlabel(" ", fontsize=8)
-g.set_title('Sparsity=0.9, Seq_len=4096, num_h=4')
+g.set_title('Sparsity=0.95, Seq_len=8192, num_h=8')
 plt.setp(g.get_legend().get_texts(), fontsize='6')
 plt.setp(g.get_legend().get_title(), fontsize='6')
-g.figure.savefig('./figs/Figure16-a.pdf')
-
+g.figure.savefig('./figs/Figure16-h.pdf')
